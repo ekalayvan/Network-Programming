6f92a628a6058b04aca5a94ce99afd736a247302
கமல் <ekalayvan@users.noreply.github.com>
2017-03-04 22:40:38 +0530

    Update tcp_echo_client.c

--- tcp_echo_client.c
+++ tcp_echo_client.c
@@ -5,25 +5,25 @@
 #include<netdb.h>
 #include<arpa/inet.h>
 #include<unistd.h>
-#define SERV_TCP_PORT 5035
-int main(int argc,char*argv[])
+#define SERV_TCP_PORT 5035 // server எந்த portல listen பண்ணுதோ அந்த port நம்பர்
+int main()
 {
 int sockfd;
 struct sockaddr_in serv_addr;
-struct hostent *server;
-char buffer[4096];
+char r_buffer[20],w_buffer[20]; // user, terminalல டைப் பண்றது இந்த bufferல தான் store ஆகும் 
 sockfd=socket(AF_INET,SOCK_STREAM,0);
-serv_addr.sin_family=AF_INET;
-serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
-serv_addr.sin_port=htons(SERV_TCP_PORT);
-printf("\nReady for sending...");
+serv_addr.sin_family=AF_INET; // IPv4கான address family
+serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1"); // server client ரெண்டும் ஒரே machineல இருக்கு, அதனால loopback address use பண்றோம்
+serv_addr.sin_port=htons(SERV_TCP_PORT); // host byteல இருந்து network byteக்கு convert பண்ணும்
+printf("\nReady for connect...");
 connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr));
 printf("\nEnter the message to send\n");
-printf("\nClient: ");
-fgets(buffer,4096,stdin);
-write(sockfd,buffer,4096);
-printf("Serverecho:%s",buffer);
+printf("\nClient : ");
+fgets(buffer,20,stdin); //user terminalல டைப் பண்றத bufferல store பண்ணும்
+write(w_sockfd,buffer,20); // server dataவ bufferல இருந்து socketக்கு அனுப்பும்
+read(sockfd,r_buffer,20)
+printf("Server echoes : %s",r_buffer);
 printf("\n");
-close(sockfd);
+close(sock_fd);  // socket close ஆகுது
 return 0;
 }
